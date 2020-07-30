b1e6657755be3ba8cb3aa9b169d51f2db1acd14b
JoaoCarlosAzevedo <jcorreadeazevedo@gmail.com>
2020-07-30 07:35:09 -0400

    Add proptypes and eslint

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
-function FormField({ label, type, name, value, onChange }) {
-    return (
-      <div>
-        <label>
-          {label}: 
-          <input
-            type={type}
-            value={value}
-            name={name}
-            onChange={onChange}
-          />
-        </label>
-      </div>
-    )
-  }
-  
-  export default FormField;
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  const fieldId = `id_${name}`;
+  return (
+    <div>
+      <label
+        htmlFor={fieldId}
+      >
+        {label}
+        :
+        <input
+          id={fieldId}
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default FormField;
